8891b9036ffc4cca62d594c1e3e4aa3e2e28e3c7
Abhaymodi860 <modiabhay860@gmail.com>
2024-11-02 12:22:58 +0530

    grid added

---
+++ Array-Minesweeper/header/Gameplay/Board/BoardView.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "../../header/UI/UIElement/ImageView.h"
+
+namespace Gameplay
+{
+    namespace Board
+    {
+        class BoardController;
+        class ImageView;
+
+        class BoardView
+        {
+        private:
+            BoardController* board_controller;
+            ImageView* background_image;
+            ImageView* board_image;
+
+            const float background_alpha = 85.f;
+            const float board_width = 866.f;
+            const float board_height = 1080.f;
+
+            void initializeBackgroundImage();
+            void initializeBoardImage();
+
+        public:
+            BoardView(BoardController* controller);
+            ~BoardView();
+
+            void initialize();
+            void update();
+            void render();
+
+        };
+    }
+}
