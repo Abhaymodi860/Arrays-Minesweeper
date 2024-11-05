fe10b8e1abe7959f5f8f3f57a316b40dd5ee022e
Abhaymodi860 <modiabhay860@gmail.com>
2024-11-05 21:08:02 +0530

    commit

--- Array-Minesweeper/header/Gameplay/GameplayService.h
+++ Array-Minesweeper/header/Gameplay/GameplayService.h
@@ -1,14 +1,30 @@
 #pragma once
+#include "../../header/Gameplay/GameplayController.h"
+#include "../../header/Gameplay/Cell/CellController.h"
+#include "../../header/UI/UIElement/ButtonView.h"
 
-namespace Gameplay {
-	class GameplayService {
+namespace Gameplay
+{
+	class GameplayService
+	{
 	private:
+		GameplayController* gameplay_controller;
 
+		void createController();
+		void destroy();
 
 	public:
+		GameplayService();
+		~GameplayService();
+
 		void initialize();
 		void update();
 		void render();
-		void reset();
+
+		void startGame();
+		void endGame(GameResult result);
+
+		int getMinesCount();
+		float getRemainingTime();
 	};
 }
